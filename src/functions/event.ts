1f6ffba5d0c1f2dc9bfb684c59cdf8b75cb85ff8
Juan Pujol <jp@beakyn.com>
2020-04-28 14:46:16 -0300

    feat(event): log file id

--- src/functions/event.ts
+++ src/functions/event.ts
@@ -4,24 +4,27 @@ import {
 export const eventCatch = async (event) => {
   const body = event.body ? JSON.parse(event.body) : null;
 
-  try {
-    console.log(JSON.stringify(body))
+  console.log(JSON.stringify(body));
 
+  try {
     // Return Challenge if asked.
-    if (body.event.type === 'url_verification')
+    if (body?.type === 'url_verification')
       return formatResponseObject({challenge: body.challenge});
 
     /**
      * Get file data to analyze
      */
-    const fileId = body.event?.type === 'file_shared' ? body.event.file_id : null;
+
+    console.log(JSON.stringify(body.event))
+
+    const fileId = body.event.type === 'file_shared' ? body.event.file_id : null;
 
     if (fileId) {
       console.log('File ID ==> ', fileId);
       // Download file...
     }
 
-    formatResponseObject(body);
+    return formatResponseObject(body);
   } catch (error) {
     console.error(error);
 
